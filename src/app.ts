7a12cef9f60bb7c784df3682f5207af63571f411
Noteb <pabloandressilva99@gmial.com>
2026-02-19 14:18:19 -0300

    ajustes

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,20 @@ import reportRoutes from "./routes/report.routes";
 export const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+// Configuración de CORS mejorada
+app.use(cors({
+  // Reemplaza esta URL con la que te dio Vercel
+  origin: [
+    "https://frontend-gym-asdev.vercel.app", 
+    "http://localhost:5173" // Mantenemos el local para que puedas seguir probando en tu PC
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use("/users", userRoutes);
-app.use("/socios", socioRoutes );
+app.use("/socios", socioRoutes);
 app.use("/shares", shareRoutes);
 app.use("/payments", paymentRoutes);
 app.use("/reports", reportRoutes);
